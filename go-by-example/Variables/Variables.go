ca6918b0a7f2d57cf1245aa20b3bdb6a4c4b1fd8
Jaay06 <josekoyi9@gmail.com>
2025-03-15 22:48:11 +0100

    study: go examples

---
+++ go-by-example/Variables/Variables.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+
+var f string = "banana"
+
+
+func main(){
+
+	var a = "Inital"
+	fmt.Println(a)
+
+	var b,c int = 1,2
+	fmt.Println(b + c)
+	
+	var d bool = true
+	fmt.Println(d)
+
+	var e int
+	fmt.Println(e)
+
+	fmt.Println(f)
+
+}
